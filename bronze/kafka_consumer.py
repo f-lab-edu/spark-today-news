37761bdabe658d06f84aacf93648ef6bd04cebe3
Dongwoo Kang <dongwoomail@icloud.com>
2023-08-12 06:15:47 +0900

    fix : change crawling data
    id, head, date, genre, press, html

--- bronze/kafka_consumer.py
+++ bronze/kafka_consumer.py
@@ -1,7 +1,8 @@
 from kafka import KafkaConsumer
+import time
 from json import loads
 # brokers = ['0.0.0.0:9092']
-brokers = ['175.45.203.105:65535']
+broker = '192.168.45.222:9092'
 
 # set kafka consumer
 consumer = KafkaConsumer(
@@ -9,7 +10,7 @@ consumer = KafkaConsumer(
     enable_auto_commit=True, # 완료되었을 때 문자 전송
     # auto_offset_reset="earliest", # 어디서부터 값을 읽어올지 (earlest 가장 처음 latest는 가장 최근)
     # group_id='my-group', # 그룹핑하여 토픽 지정할 수 있다 (같은 컨슈머로 작업)
-    bootstrap_servers=brokers, # 브로커 주소: 포트번호
+    bootstrap_servers=broker, # 브로커 주소: 포트번호
     api_version = (0,11,5),
     value_deserializer=lambda v: loads(v.decode('utf-8')), # 역직렬화 ( 받을 떄 )
     # consumer_timeout_ms=1000 # 1000초가 지나면 없는 것으로 취급한다.
@@ -17,6 +18,8 @@ consumer = KafkaConsumer(
 
 
 for news in consumer:
-    print(f"{news.value['head', 'date', 'genre']}")
+    print(news.value)
+
+
 
 consumer.close()
--- bronze/kafka_producer.py
+++ bronze/kafka_producer.py
@@ -13,12 +13,12 @@ from bs4 import BeautifulSoup as bs
 # Kafka
 from kafka import KafkaProducer
 # brokers = ['0.0.0.0:9092']
-brokers = ['175.45.203.105:65535']
+broker = '192.168.45.222:9092'
 # set kafka producer
 producer = KafkaProducer(
     acks=0, # 메시지 받는 사람이 잘  받았는지 체크하는 옵션 (0은 확인 없이그냥 보내기)
     compression_type='gzip', # 메시지 전달할 때  압축
-    bootstrap_servers=brokers, # 브로커 주소: 포트번호
+    bootstrap_servers=broker, # 브로커 주소: 포트번호
     value_serializer=lambda v: dumps(v).encode('utf-8'), # 데이터 전송을 위해 byte 단위로 바꿔주는 작업
     api_version = (0,11,5)
 )
@@ -31,12 +31,6 @@ date = dt.datetime.now(tz_asia_seoul).strftime('%Y%m%d')
 header = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5.1 Safari/605.1.15"}
 genres = {'100':'정치', '101':'경제', '102':'사회', '103':'문화'}
 
-# out-string
-rm_string = '''
-.,"“”‘’[]\|?!
-'''
-tab_string = '·…'
-
 
 while True:
     for genre in genres.keys():
@@ -47,13 +41,16 @@ while True:
             time.sleep(2)
             html = rep.text
             soup = bs(html, "html.parser")
-
             headlines = soup.select("dt:nth-child(2) > a")
             dates = soup.select("span.date.is_new")
-
+            presses = soup.select("span.writing")
             for i in range(3):
                 try: 
+                    url = headlines[i]['href']
+                    id = url.split("?")[0].replace("https://n.news.naver.com/mnews/article/", "").replace("/", "-")
                     headline = headlines[i].get_text(strip=True)
+                    press = presses[i].get_text(strip=True)
+                    subhtml = req.get(url).text
                     date = dates[i].get_text()
                 except:
                     continue
@@ -62,23 +59,20 @@ while True:
                     continue
                 
                 # 2분전 데이터를 가져옵니다.
-                if int(date.split('분전')[0]) < 10:
-
-                    for rm in rm_string:
-                        headline = headline.replace(rm, '')
-                    for tab in tab_string:
-                        headline = headline.replace(tab, ' ')
-                
+                if int(date.split('분전')[0]) < 2:
                     times = dt.datetime.now(tz_asia_seoul).strftime('%Y%m%d %H:%M:%S')
                     gen = genres[genre]
                     
-                    print(headline, times, gen)
+                    print(id, headline, times, gen, press, subhtml)
 
                     # 토픽(news)와 메세지 보내기
                     producer.send('new-topic', {
+                        'id' : id,
                         'head' : headline,
                         'date' : times,
-                        'genre' : gen
+                        'genre' : gen,
+                        'press' : press,
+                        'html' : subhtml
                     })
                     producer.flush() # 데이터 비우기
 
